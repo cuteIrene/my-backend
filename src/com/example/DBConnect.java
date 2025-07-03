7ec855cc1d366eb09dad93234ee9ff46b37e2abf
Orsino Tseng <dodo870118@gmail.com>
2025-07-04 02:45:26 +0800

    註解本機段

--- src/com/example/DBConnect.java
+++ src/com/example/DBConnect.java
@@ -14,10 +14,11 @@ public class DBConnect {
         String jdbcUrl = "jdbc:mysql://" + host + ":" + port + "/" + db + "?useSSL=false&serverTimezone=UTC";
         return DriverManager.getConnection(jdbcUrl, user, pass);
     }
+    /* 
     // MySQL 連線資訊
     public static final String URL = "jdbc:mysql://localhost:3306/beautifullife";
     public static final String USER = "root";
-    public static final String PASSWORD = "Irene870118";
+    public static final String PASSWORD = "Irene870118";*/
 
     static {
         try {
